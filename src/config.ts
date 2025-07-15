93126c5a328273237c5f9568aa34022541e60369
maayan <maayansavir@gmail.com>
2025-07-15 18:00:20 -0400

    use build production endpoint

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ dotenv.config();
 export const config = {
   aptos_build: {
     botKey: process.env.APTOS_BOT_KEY,
-    adminUrl: "https://admin.api.staging.aptoslabs.com/api/rspc",
+    adminUrl: "https://admin.api.aptoslabs.com/api/rspc",
   },
   server: {
     name: "Aptos MCP Server",
